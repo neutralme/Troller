3bdce65333777804b47a586c13bdadf5d0bdec0c
neutralme <neutral0me@gmail.com>
2021-06-12 20:56:39 +0545

    Updated

--- js/main1.js
+++ js/main1.js
@@ -2,11 +2,11 @@ let copy = document.getElementById("copy");
 let code = document.getElementsByClassName("text")[0];
 let result = document.getElementById("result");
 let generate = document.getElementById("generate");
-const generateURL = (nameValue, adjValue) => {
+const generateURL = () => {
   code.innerText = "https://neutralme.github.io/Troller/index.html?name=" + btoa(document.getElementById("name").value) + "&adj=" + btoa(document.getElementById("adj").value);
   result.style.display = "block";
 }
-const copyURL = (copyValue) => {
+const copyURL = () => {
   notify.style.display = "block";
   code.select();
   code.setSelectionRange(0, 99999)
